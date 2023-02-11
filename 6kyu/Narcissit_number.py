7cfff6e08e1b54e38063f238f8c2115be5a4ef37
Juan Cruz <juansarde@gmail.com>
2023-02-11 19:50:21 -0300

    New 6kyu Challenge

---
+++ 6kyu/Narcissit_number.py
@@ -0,0 +1,26 @@
+# A Narcissistic Number (or Armstrong Number) 
+# is a positive number which is the sum of its own digits, 
+# each raised to the power of the number of digits in a given base. 
+# In this Kata, we will restrict ourselves to decimal (base 10).
+
+# For example, take 153 (3 digits), which is narcissistic:
+#     1^3 + 5^3 + 3^3 = 1 + 125 + 27 = 153
+# and 1652 (4 digits), which isn't:
+#     1^4 + 6^4 + 5^4 + 2^4 = 1 + 1296 + 625 + 16 = 1938
+# The Challenge:
+# Your code must return true or false (not 'true' and 'false') depending upon whether the given number is a Narcissistic number in base 10. This may be True and False in your language, e.g. PHP.
+# Error checking for text strings or other invalid inputs is not required, only valid positive non-zero integers will be passed into the function.
+
+def narcissistic( value ):
+    number = 0
+    string_value = str(value)
+    range_value = len(string_value)
+
+    for i in range(range_value):
+        temporal_number = int(string_value[i])
+        number = number + (temporal_number**range_value)
+
+    return True if number == value else False
+
+
+print(narcissistic(13))
